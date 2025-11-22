953acee09bfd901fc9ee08f552e79069fad7fd6a
shanthappa-sappati <shanthappa.sappati@gmail.com>
2025-11-22 17:19:39 +0530

    Changes to added new htlm pages

--- src/main/java/com/surya/carpool/controller/WebController.java
+++ src/main/java/com/surya/carpool/controller/WebController.java
@@ -1,49 +1,61 @@
 package com.surya.carpool.controller;
 
-import com.surya.carpool.model.Ride;
-import com.surya.carpool.model.User;
-import com.surya.carpool.repository.RideRepository;
-import com.surya.carpool.repository.UserRepository;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.List;
+import com.surya.carpool.model.Ride;
+import com.surya.carpool.model.User;
+import com.surya.carpool.repository.RideRepository;
+import com.surya.carpool.repository.UserRepository;
 
 @Controller
 public class WebController {
 
-    private final RideRepository rideRepo;
-    private final UserRepository userRepo;
+	private final RideRepository rideRepo;
+	private final UserRepository userRepo;
 
-    public WebController(RideRepository rideRepo, UserRepository userRepo) {
-        this.rideRepo = rideRepo;
-        this.userRepo = userRepo;
-    }
+	public WebController(RideRepository rideRepo, UserRepository userRepo) {
+		this.rideRepo = rideRepo;
+		this.userRepo = userRepo;
+	}
 
-    @GetMapping({"/", "/home"})
-    public String home(Model model) {
-        List<Ride> rides = rideRepo.findAll();
-        model.addAttribute("rides", rides);
-        return "index";
-    }
+	@GetMapping({ "/", "/home" })
+	public String home(Model model) {
+		List<Ride> rides = rideRepo.findAll();
+		model.addAttribute("rides", rides);
+		return "index";
+	}
 
-    @GetMapping("/users/ui")
-    public String users(Model model) {
-        List<User> users = userRepo.findAll();
-        model.addAttribute("users", users);
-        return "users";
-    }
+	@GetMapping("/users/ui")
+	public String users(Model model) {
+		List<User> users = userRepo.findAll();
+		model.addAttribute("users", users);
+		return "users";
+	}
 
-    @GetMapping("/rides/ui")
-    public String rides(Model model) {
-        List<Ride> rides = rideRepo.findAll();
-        model.addAttribute("rides", rides);
-        return "rides";
-    }
+	@GetMapping("/rides/ui")
+	public String rides(Model model) {
+		List<Ride> rides = rideRepo.findAll();
+		model.addAttribute("rides", rides);
+		return "rides";
+	}
 
-    @GetMapping("/bookings/ui")
-    public String bookings() {
-        return "bookings";
-    }
+	@GetMapping("/bookings/ui")
+	public String bookings() {
+		return "bookings";
+	}
+
+	@GetMapping("/aboutus/ui")
+	public String aboutus() {
+		return "aboutus";
+	}
+
+	@GetMapping("/contactus/ui")
+	public String contactus() {
+		return "contactus";
+	}
+	
 }
